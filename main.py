278aed09239eef87b7a2e8e2e2efa0d7bdcbcda1
FrannyBird <80104386+FrannyBird@users.noreply.github.com>
2021-10-17 14:59:27 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+print('\n'.join([''.join([('ImSubmissive&Breedable'[(x-y) % len('You'reAmazing!')] if ((x*0.045)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ' if y > -11 else '') for x in range(-30,30)]) for y in range(30,-30,-1)]))
